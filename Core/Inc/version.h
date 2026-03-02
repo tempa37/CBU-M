29a95b893d02a7f86037ab06032bb007cc1ecf41
Nikita <ron05051980@gmail.com>
2026-03-02 20:27:52 +0700

    rrr

--- Core/Inc/version.h
+++ Core/Inc/version.h
@@ -6,13 +6,13 @@
 // x.X.x: Minor version of the stack
 #define VERSION_MINOR 4
 // x.x.X: Build of the stack
-#define VERSION_BUILD 222
+#define VERSION_BUILD 225
 
 // Provides the version
 #define FW_VERSION ((VERSION_MAJOR) << 24 | \
                        (VERSION_MINOR) << 16 | \
                        (VERSION_BUILD) << 8)
 
-#define BUILD_DATE "02/12/2026, 18:28:03"
+#define BUILD_DATE "03/02/2026, 20:24:37"
 
 #endif /* __VERSION_H__ */
